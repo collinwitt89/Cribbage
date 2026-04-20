4d08dbc902f18cdec2fb5422f3997bfe9a2a0b15
collinwitt89 <collinwitt89@gmail.com>
2026-04-20 19:48:14 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from “react”;
+import { useState, useEffect } from "react";
 
 function uid() { return Math.random().toString(36).slice(2) + Date.now(); }
 function fmt(date) {
